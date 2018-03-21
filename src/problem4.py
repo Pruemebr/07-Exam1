6595bf1d8423c3d5ae8f971184a36715ebe49b4f
pruemebr <pruemebr@rose-hulman.edu>
2018-03-20 22:15:43 -0400

    Test Problem 4

--- src/problem4.py
+++ src/problem4.py
@@ -2,8 +2,8 @@
 Exam 1, problem 4.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Bryce Pruemer.  March 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -134,8 +134,15 @@ def problem4(m):
       -- problem4(97)   returns 0 because 97 has no non-trivial factors
                                  (i.e., it is prime)
     """
+    count = 0
+    for k in range(m):
+        if k != 0:
+            if k != 1:
+                if m % k == 0:
+                    count = count + 1
+    return count
     # -------------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # Done: 7. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
